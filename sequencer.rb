1aeddd5738314a36f40ac984bf865e8437afa360
lukew244 <luke.webster86@gmail.com>
2018-04-22 13:45:56 +0100

    sequencer refactor

--- sequencer.rb
+++ sequencer.rb
@@ -4,16 +4,13 @@ require_relative './environment'
 
 class Sequencer
 
-  #start position [0,0,'N'], instructions "RLRL"
-  def process(start_position, instructions)
-    grid = [5, 3]
-    environment = Environment.new(grid)
-    robot = Robot.new(start_position, environment)
+  def process(grid, instruction_sets, environment = Environment.new(grid))
+    instruction_sets.map { |set| process_set(set, environment) }
+  end
 
-    dir = instructions.split("")
-    dir.each_with_object(robot) do |inst, robot|
-      robot.move(inst)
-    end
-    robot.to_array
+  def process_set(set, environment)
+    start_position, instructions = set[0], set[1].split("")
+    robot = Robot.new(start_position, environment)
+    robot.process_moves(instructions)
   end
 end
--- spec/sequencer_spec.rb
+++ spec/sequencer_spec.rb
@@ -2,19 +2,10 @@ require "./sequencer"
 
 RSpec.describe Sequencer do
 
-  it "handles orientation of robot from instructions" do
-    start_position = [0,0, "N"]
-    expect(subject.process(start_position, "RRR")).to eq [0, 0, "W"]
-    expect(subject.process(start_position, "RRLLRLR")).to eq [0, 0, "E"]
-  end
-
-  it "handles movement of robot from instructions" do
-    expect(subject.process([1,1, "E"], "F")).to eq [2, 1, "E"]
-    expect(subject.process([1,1, "E"], "RFRFRFRF")).to eq [1, 1, "E"]
-  end
-
-  it "knows when a robot is lost" do
-    expect(subject.process([3,2, "N"], "FRRFLLFFRRFLL")).to eq [3, 3, "N", "LOST"]
-    # expect(subject.process([0,3, "W"], "LLFFFLFLFL")).to eq [2, 3, "S"] #
+  it "processes sequence of instructions" do
+    grid = [5,3]
+    robots = [[[3,2, "N"], "FRRFLLFFRRFLL"], [[1,1, "E"], "F"], [[0,3, "W"], "LLFFFLFLFL"]]
+    output = [[3, 3, "N", "LOST"], [2, 1, "E"], [2, 3, "S"]]
+    expect(subject.process(grid, robots)).to eq output
   end
 end
